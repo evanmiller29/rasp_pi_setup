f15a79c9d6c6219cf6ce7f67596a0365969bcfc4
Evan Miller <evanmiller29@gmail.com>
2020-06-16 19:03:52 +0100

    Adding in NordVPN/deluge

--- install_games_media.sh
+++ install_games_media.sh
@@ -1,11 +1,13 @@
-# Page https://ubuntu.pkgs.org/20.04/ubuntu-multiverse-arm64/vcmi_0.99+dfsg+git20190113.f06c8a87-1_arm64.deb.html
-
 sudo apt update
 sudo apt autoclean
 
 cd ..
 mkdir installed_shiz
 cd installed_shiz
+
+################################
+###### HEROES MOTHERUCKER ######
+################################
 
 mkdir heroes_3 -p
 cd heroes_3
@@ -24,18 +26,44 @@ sudo apt-get install innoextract
 # Run once you've downloaded heroes launcher from GoG
 # vcmibuilder --gog setup_homm3_complete_2.0.0.16.exe
 
-# Setting up kodi dependencies
+################################
+# Setting up kodi dependencies #
+################################
+
 # https://pimylifeup.com/raspberry-pi-netflix/
 cd ..
 mkdir kodi -p
 cd kodi
 
+#sudo apt install kodi kodi-peripheral-joystick kodi-pvr-iptvsimple kodi-inputstream-adaptive kodi-inputstream-rtmp
+#sudo apt install build-essential python-pip python-dev libffi-dev libssl-dev libnss3
+
+#sudo pip install setuptools wheel
+#sudo pip install pycryptodomex win_inet_pton
+#wget https://github.com/castagnait/repository.castagnait/raw/master/repository.castagnait-1.0.1.zip
+
+cd ..
+
+################################
+########### NordVPN ############
+################################
+
+mkdir nordvpn -p
+cd nordvpn
+
+sudo wget  wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
+sudo dpkg -i nordvpn-release_1.0.0_all.deb
+
 sudo apt update
-sudo apt upgrade
-sudo apt install kodi kodi-peripheral-joystick kodi-pvr-iptvsimple kodi-inputstream-adaptive kodi-inputstream-rtmp
-sudo apt install build-essential python-pip python-dev libffi-dev libssl-dev libnss3
+sudo apt install nordvpn -y
 
-sudo pip install setuptools wheel
-sudo pip install pycryptodomex win_inet_pton
-wget https://github.com/castagnait/repository.castagnait/raw/master/repository.castagnait-1.0.1.zip
+nordvpn login
+# Make sure that nordvnp connect is added to restart script
+# @reboot nordvpn connect
+crontab -e
 
+################################
+############ Deluge ############
+################################
+
+sudo apt-get install deluge
