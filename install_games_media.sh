66a4ed8a3f18195a1db67bacc71c26b9878cd3f6
Evan Miller <evanmiller29@gmail.com>
2020-06-18 02:59:20 -0500

    Adding flags to installers, stoppping kodi for time being

--- install_games_media.sh
+++ install_games_media.sh
@@ -22,18 +22,20 @@ sudo dpkg -i vcmi_0.99+dfsg+git20190113.f06c8a87-1_armhf.deb
 sudo apt install ./vcmi_0.99+dfsg+git20190113.f06c8a87-1_armhf.deb
 sudo apt --fix-broken install -y
 
-sudo apt-get install innoextract
+sudo apt-get install innoextract -y
 # Run once you've downloaded heroes launcher from GoG
 # vcmibuilder --gog setup_homm3_complete_2.0.0.16.exe
+
+cd ..
 
 ################################
 # Setting up kodi dependencies #
 ################################
 
 # https://pimylifeup.com/raspberry-pi-netflix/
-cd ..
-mkdir kodi -p
-cd kodi
+#cd ..
+#mkdir kodi -p
+#cd kodi
 
 #sudo apt install kodi kodi-peripheral-joystick kodi-pvr-iptvsimple kodi-inputstream-adaptive kodi-inputstream-rtmp
 #sudo apt install build-essential python-pip python-dev libffi-dev libssl-dev libnss3
@@ -41,8 +43,6 @@ cd kodi
 #sudo pip install setuptools wheel
 #sudo pip install pycryptodomex win_inet_pton
 #wget https://github.com/castagnait/repository.castagnait/raw/master/repository.castagnait-1.0.1.zip
-
-cd ..
 
 ################################
 ########### NordVPN ############
@@ -67,4 +67,4 @@ sudo nano /etc/rc.local
 ############ Deluge ############
 ################################
 
-sudo apt-get install deluge
+sudo apt-get install deluge -y
